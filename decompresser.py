37fd5417ae6f58f2c8711e4fd26c30346dc5d175
Hamish Briggs <119870385+Something231@users.noreply.github.com>
2023-09-06 12:32:46 +0800

    Update decompresser.py

--- decompresser.py
+++ decompresser.py
@@ -19,6 +19,14 @@ for word in words:
             position = val_list.index(word)
             word = key_list[position]
             decompressed = f"{decompressed}{word} "
+        elif word.startswith("#") == True:
+            word = word.replace("#", "")
+            word = int(word)
+            for child in range(word):
+                decompressed = f"{decompressed} "
+        elif "$" in word:
+            wordz = word.split("$")
+            decompressed = f"{decompressed}{wordz[0]} {wordz[1]} "
         else:
             decompressed = f"{decompressed}error "
     else:
@@ -34,6 +42,7 @@ decompressed = decompressed.replace("$6", "6")
 decompressed = decompressed.replace("$7", "7")
 decompressed = decompressed.replace("$8", "8")
 decompressed = decompressed.replace("$9", "9")
+
 
 with open("decompressed.txt", 'w') as text_file:
     text_file.write(decompressed)
